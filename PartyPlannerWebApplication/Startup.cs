3063dc8f03c06fdfaf5024482622479dc0fe10a0
Adam Goddard <afgoddard89@gmail.com>
2015-10-06 20:12:57 +0100

    Intital Project Upload to Repo
    Working Project for Visual Studio

---
+++ PartyPlannerWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PartyPlannerWebApplication.Startup))]
+namespace PartyPlannerWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
